854979f7c800bf94da5d623f6b5f6a2b81664ca6
Abduraxmonov <126749705+Avazbek-Abduraxmonov@users.noreply.github.com>
2023-09-12 23:19:43 +0500

    python-practice

---
+++ python.py
@@ -0,0 +1,59 @@
+# def function(a):
+# 	result = []
+
+# 	for i in a:
+# 		# print(i)
+# 		try:
+# 			i = i**2
+# 			result.append(i)
+
+# 		except:
+# 			pass
+			
+
+# 	return result
+
+# x = function([1 , 2, 5,'asadsad'])
+
+# print(f"Natija: {x}")
+
+
+
+# # numbers = [3, 4, 6, 'sdasda']
+
+# # results = []
+
+# # for i in numbers:
+# #     try:
+# #         result = i ** 2
+# #         results.append(result)
+# #     except TypeError:
+# #         pass
+
+# # print(results)
+
+# def find_average(arr):
+#     sum = 0
+#     for num in arr:
+#         sum += num
+#     average = sum / len(arr)
+#     return average
+
+
+# numbers = [5, 10, 15, 20]
+# average = find_average(numbers)
+# print("Natija", average)
+
+
+# # algoritm
+
+
+# numbers = [5,10,15,20]
+
+# sum = 0
+
+# for i in numbers:
+# 	sum+=i
+# average = sum / len(numbers)
+
+# print(f"Natija : {average}")
